f39fcb7412583ffbef2114fff212b472efb61bd6
IVarenikov <v71196001@gamil.com>
2025-01-13 16:53:37 +0200

    https://kompege.ru/variant?kim=25073855

---
+++ 08-01-25/Task-2.py
@@ -0,0 +1,12 @@
+print('x y x z w ')
+for x in range(2):
+    for y in range(2):
+        for z in range(2):
+            for w in range(2):
+                f = (not (w <= z)) or (x <= y) or (not X)
+
+
+
+
+
+
