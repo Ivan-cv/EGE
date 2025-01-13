7240a5f556021c008ffcbf1808f6a91a4c191cec
IVarenikov <v71196001@gamil.com>
2025-01-13 17:35:15 +0200

    https://kompege.ru/variant?kim=25073855

--- 08-01-25/Task-2.py
+++ 08-01-25/Task-2.py
@@ -1,12 +1,13 @@
-print('x y x z w ')
-for x in range(2):
-    for y in range(2):
-        for z in range(2):
-            for w in range(2):
-                f = (not (w <= z)) or (x <= y) or (not X)
+from itertools import permutations, product
 
 
+def f( x, y, z, w):
+    return ((x <= z) <= y ) or (not w)
 
-
-
-
+for a1, a2, a3, a4, a5, a6, a7 in product([1,0],repeat=7):
+    table = [(1,0,a1, a2),(a3, 1, 0, a4),(0, a5, a6, a7)]
+    if len(table) == len(set(table)):
+        for p in permutations('xyzw'):
+            u = [f(**dict(zip(p,t)))for t in table] == [0,0,0]
+            if u:
+                print(*p)
