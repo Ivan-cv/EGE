c5852bbe2caf8ed6547c8ff2eb66efa6b2a823ef
IVarenikov <v71196001@gamil.com>
2025-02-20 17:30:25 +0200

    .

---
+++ 17-02-25/Task-25 -4628-01.py
@@ -0,0 +1,15 @@
+from itertools import product
+ans = []
+for v in '0123456789':
+    for r in range(3):
+        for r in range(3):
+            for z in product('0123456789', repeat=r):
+                z = ''.join(z)
+
+                num = int('12' + z + '4' + 'v' + '65')
+                if num % 161 == 0:
+                    ans .append([num,num // 161])
+
+ans = sorted
+for i in ans:
+    print(*i)
---
+++ 17-02-25/Task-25-2628.02.py
@@ -0,0 +1,5 @@
+from fnmatch import fnmatch
+for i in range(161,10**8,161):
+    if fnmatch(str(i),'12*4?65'):
+        if i % 161 == 0:
+            print(i,i // 161)
